7e1eb979c03448e15e245b8a8c7b8ee029166c3a
daf1na <dafinabalaj345@gmail.com>
2023-04-15 10:50:02 +0200

    Beaufort Cipher Algorithm

--- beaufort-cipher.py
+++ beaufort-cipher.py
@@ -4,3 +4,10 @@ def beaufort_cipher(plaintext, key):
     for i in range(len(plaintext)):
         # Convert the characters to uppercase and subtract 'A' to get a value between 0 and 25
         char_value = (ord(plaintext[i].upper()) - ord('A')) % 26
+        key_value = (ord(key[i % len(key)].upper())-ord('A')) % 26
+        # Apply the Beaufort Cipher algorithm to obtain the encrypted or decrypted character
+        encrypted_char = chr(((key_value - char_value) % 26) + ord('A'))
+        ciphertext += encrypted_char
+
+    return ciphertext
+
